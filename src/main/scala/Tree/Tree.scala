cf90531b0b3f561cf1965b5315a15159a4e01b79
Sara Simon <sara.simon_ext@scout24.com>
2020-04-10 19:02:28 +0200

    Latest session

--- src/main/scala/Tree/Tree.scala
+++ src/main/scala/Tree/Tree.scala
@@ -9,10 +9,20 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 object Tree {
 
   // Ex 5. Write a function size that counts the number of nodes (leaves and branches) in a tree.
-  def size[A](tree: Tree[A]) : Int = ???
-
-  def apply[A](as: Branch[A]): Tree[A] = ???
+  def size[A](tree: Tree[A]) : Int = tree match {
+    case Branch(l, r) => size(l) + size(r) + 1
+    case Leaf(_) => 1
+  }
 
   //Ex 6. Write a function path that returns the maximum path length from the root of a tree to any leaf.
-  def maximumPath[A](tree: Tree[A], origin: Leaf[A]) : Int = ???
+  def maximumPath[A](tree: Tree[A], origin: Leaf[A]) : Int = tree match {
+    case Leaf(a) if origin.value.equals(a) => 1
+    case Leaf(_) => 0
+    case Branch(l, r) => maximumPath(l, origin) + maximumPath(r, origin)
+  }
+
+  def apply[A](as: Tree[A]): Tree[A] = as match {
+    case branch: Branch[A] => Branch(branch.left, branch.right)
+    case leaf: Leaf[A] => leaf
+  }
 }
--- src/test/scala/Tree/TreeTest.scala
+++ src/test/scala/Tree/TreeTest.scala
@@ -5,5 +5,23 @@ import org.scalatest.{BeforeAndAfterEach, MustMatchers, WordSpec}
 class TreeTest extends WordSpec with MustMatchers with BeforeAndAfterEach {
 
   "size" should {
+
+    "return the total number of nodes" in {
+      val size = Tree.size(Tree(Branch(Leaf(1), Leaf(2))))
+
+      size mustBe 3
+    }
+
+    "" in {
+      val size = Tree.size(Tree(Branch(Branch(Leaf(1), Branch(Leaf(3), Leaf(4))), Leaf(2))))
+
+      size mustBe 7
+    }
+
+    "maximum path" in {
+      val maxPath = Tree.maximumPath(Tree(Branch(Branch(Leaf(1), Branch(Branch(Leaf(5), Leaf(6)), Leaf(4))), Leaf(2))), Leaf(2))
+
+      maxPath mustBe 1
+    }
   }
 }
